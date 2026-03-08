320bddbd6038e9c911c691f9fd9930dbb1af8b01
ElliottCepin <elliottcepin5@gmail.com>
2026-03-07 17:51:02 -0700

    global state with mutex protection

--- main.go
+++ main.go
@@ -6,7 +6,12 @@ import (
 	"strconv"
 	"time"
 	"log"
+	"sync"
 )
+
+var counter_lock sync.Mutex
+var counter int = 0
+
 
 func slow(w http.ResponseWriter, r *http.Request) {
 	if (r.Method != "GET"){
@@ -14,13 +19,27 @@ func slow(w http.ResponseWriter, r *http.Request) {
 	}
 	values := r.URL.Query()
 	seconds := values.Get("seconds")
+	counter_lock.Lock()
+	counter += 1;
+	counter_lock.Unlock()
 	iseconds, _ := strconv.Atoi(seconds) // iseconds == 0 on error, which is fine.
+
 	time.Sleep(time.Duration(iseconds) * time.Second)
+	counter_lock.Lock()
+	counter -= 1
+	counter_lock.Unlock()
+
 	fmt.Fprintf(w, "Waited %v seconds", iseconds);	
+}
+
+func status(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "%v processes running", counter);	
 }
 
 func main() {
 	http.HandleFunc("/slow", slow)
+	http.HandleFunc("/status", status)
+
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
