13cb2b1671f1d594d4b2dcaaa5f57eef7db6e35a
ElliottCepin <elliottcepin5@gmail.com>
2026-03-09 17:30:50 -0700

    respects client cancellation

--- main.go
+++ main.go
@@ -15,31 +15,54 @@ var counter int = 0
 
 func slow(w http.ResponseWriter, r *http.Request) {
 	if (r.Method != "GET"){
-		// we'll talk later
+		return
 	}
-	values := r.URL.Query()
-	seconds := values.Get("seconds")
-	counter_lock.Lock()
-	counter += 1;
-	counter_lock.Unlock()
-	iseconds, _ := strconv.Atoi(seconds) // iseconds == 0 on error, which is fine.
+	ctx := r.Context()
 
-	time.Sleep(time.Duration(iseconds) * time.Second)
-	counter_lock.Lock()
-	counter -= 1
-	counter_lock.Unlock()
+	result := make(chan int, 1)
+	
+	go func() {
+		values := r.URL.Query()
+		seconds := values.Get("seconds")
+		counter_lock.Lock()
+		counter += 1;
+		counter_lock.Unlock()
+		iseconds, _ := strconv.Atoi(seconds) // iseconds == 0 on error, which is fine.
+		select {
+		case <-time.After(time.Duration(iseconds) * time.Second):
+		case <-ctx.Done():
+		}
+		
+		counter_lock.Lock()
+		counter -= 1
+		counter_lock.Unlock()
+		result <- iseconds
+	}()
+	
+	select {
+		case res := <- result:
+			fmt.Fprintf(w, "Waited %v seconds", res);	
+		case <- ctx.Done():
+			log.Print("Client timed out")
+	}
 
-	fmt.Fprintf(w, "Waited %v seconds", iseconds);	
 }
 
 func status(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "%v processes running", counter);	
+	counter_lock.Lock()
+	fmt.Fprintf(w, "%v processes running", counter);
+	counter_lock.Unlock()
 }
 
 func main() {
+	srv := &http.Server{
+		Addr:		":8080",
+		ReadTimeout:	5 * time.Second,
+		WriteTimeout:	30 * time.Second,
+	}
 	http.HandleFunc("/slow", slow)
 	http.HandleFunc("/status", status)
 
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(srv.ListenAndServe())
 }
